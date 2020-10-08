997953c6a52fadf0e4e13e7da81bcad15ee97ff2
Fredo699 <fred@fpf3.net>
2020-10-08 11:11:58 -0400

    made it so windows preserve tag when changing monitors

--- dwm.c
+++ dwm.c
@@ -1654,7 +1654,7 @@ sendmon(Client *c, Monitor *m)
 	detach(c);
 	detachstack(c);
 	c->mon = m;
-	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
+	//c->tags = m->tagset[m->seltags]; /* assign tags of target monitor (FPF3: Disabled this)*/
 	attach(c);
 	attachstack(c);
 	focus(NULL);
