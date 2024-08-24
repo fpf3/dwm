950f68b820eadf03abe5a24cc24786bc4a925744
Fred Frey <fred@fpf3.net>
2024-08-24 16:25:45 -0400

    buildInputs and nativeBuildInputs were technically swapped, but it don't really matter

--- shell.nix
+++ shell.nix
@@ -1,21 +1,23 @@
 # shell for dev and debug
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell rec {
-    buildInputs = with pkgs; [
-      dmenu
-      feh
-      gnumake
-      lukesmithxyz-st
-      rofimoji
-    ];
-
-    nativeBuildInputs = with pkgs.buildPackages;
+    buildInputs = with pkgs.buildPackages;
     [
       xorg.xev
       xorg.libX11
       xorg.libXft
       xorg.libXinerama
       xorg.libxcb
+    ];
+    
+    nativeBuildInputs = with pkgs; [
+      dmenu
+      feh
+      gnumake
+      lukesmithxyz-st
+      rofimoji
+      libclang
+      clang-tools
     ];
     
     LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
