c90703a53bf4206cb78816f27b426e24ceddafa0
Fred Frey <fred@fpf3.net>
2025-11-28 16:32:58 -0500

    case insensitive search

--- config.h
+++ config.h
@@ -110,7 +110,7 @@ const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *mutetogglecmd[] = { "wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", NULL };
